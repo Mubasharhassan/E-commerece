490504eaef06d8620d4f781a4a08dd5b57df4cb0
AshirDev <mubashar0311720@gmail.com>
2022-09-19 16:12:15 +0500

    add something

--- src/Components/FashionProduct.js
+++ src/Components/FashionProduct.js
@@ -5,71 +5,73 @@ const FashionProduct = () => {
   return (
     <>
       <div className="container-fluid">
-        <div class="d-flex align-items-center flex-wrap-wrap">
+        <div class="d-flex align-items-center main-cards-detail">
           <div className="children-div">
-            <div className="cards-body">
-              <Card body className="text-center outerwear">
+            <div>
+              <Card body className="text-end my-2 outerwear">
                 <div className="cards-detail">
                   <CardTitle tag="h5">Special Title Treatment</CardTitle>
-                  <CardText className="outerwear-pera">outerwear</CardText>
-                  <h2>New</h2>
-                  <CardText className="collection">Collection</CardText>
-                  <Button color="">Shop Now</Button>
+                  <CardText>
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </CardText>
+                  <Button color="primary">Go somewhere</Button>
                 </div>
               </Card>
             </div>
-            <div className="cards-body">
-              <Card body className="text-end my-2 summer-hot">
+            <div>
+              <Card body className="text-end my-2 summer-hot  ">
                 <div className="cards-detail">
                   <CardTitle tag="h5">Special Title Treatment</CardTitle>
-                  <CardText className="outerwear-pera">outerwear</CardText>
-                  <h2>New</h2>
-                  <CardText className="collection">Collection</CardText>
-                  <Button color="">Shop Now</Button>
+                  <CardText>
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </CardText>
+                  <Button color="primary">Go somewhere</Button>
                 </div>
               </Card>
             </div>
           </div>
           <div className="children-div">
-            <div className="cards-body">
-              <Card body className="text-end my-2 selected-offer">
-                <div className="cards-detail">
-                  <CardTitle tag="h5">Special Title Treatment</CardTitle>
-                  <CardText className="outerwear-pera">outerwear</CardText>
-                  <h2>New</h2>
-                  <CardText className="collection">Collection</CardText>
-                  <Button color="">Shop Now</Button>
-                </div>
-              </Card>
-            </div>
-          </div>
-        </div>
-        <div className="children-div">
-          <div className="cards-body">
-            <Card body className="text-center outerwear">
+            <Card body className="text-end my-2 selected-offer">
               <div className="cards-detail">
                 <CardTitle tag="h5">Special Title Treatment</CardTitle>
-                <CardText className="outerwear-pera">outerwear</CardText>
-                <h2>New</h2>
-                <CardText className="collection">Collection</CardText>
-                <Button color="">Shop Now</Button>
+                <CardText>
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </CardText>
+                <Button color="primary">Go somewhere</Button>
               </div>
             </Card>
           </div>
-          <div className="cards-body">
-            <Card body className="text-end my-2 summer-hot">
-              <div className="cards-detail">
-                <CardTitle tag="h5">Special Title Treatment</CardTitle>
-                <CardText className="outerwear-pera">outerwear</CardText>
-                <h2>New</h2>
-                <CardText className="collection">Collection</CardText>
-                <Button color="">Shop Now</Button>
-              </div>
-            </Card>
+          <div className="children-div">
+            <div>
+              <Card body className="text-end my-2 arrivals">
+                <div className="cards-detail">
+                  <CardTitle tag="h5">Special Title Treatment</CardTitle>
+                  <CardText>
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </CardText>
+                  <Button color="primary">Go somewhere</Button>
+                </div>
+              </Card>
+            </div>
+            <div>
+              <Card body className="text-end my-2 hot-offer">
+                <div className="cards-detail">
+                  <CardTitle tag="h5">Special Title Treatment</CardTitle>
+                  <CardText>
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </CardText>
+                  <Button color="primary">Go somewhere</Button>
+                </div>
+              </Card>
+            </div>
           </div>
         </div>
       </div>
-      {/* </div> */}
     </>
   );
 };
