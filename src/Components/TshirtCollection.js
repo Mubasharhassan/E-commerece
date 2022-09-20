155eeaa63d63f76b379d853ac32e03a8a1020bfc
AshirDev <mubashar0311720@gmail.com>
2022-09-20 15:19:17 +0500

    add

--- src/Components/TshirtCollection.js
+++ src/Components/TshirtCollection.js
@@ -3,36 +3,27 @@ import React from "react";
 const TshirtCollection = () => {
   return (
     <>
-      <div className="container-fluid mt-5 ">
-        <div className="tshirt-colloction d-flex justify-content-between">
+      <div className="container-fluid d-flex justify-content-between">
+        <section className="tshirt-colloction">
           <div>
             <div className="design-days-men">
-              <h3>00</h3>
-              <p>days</p>
+              <p className="deta-detail-pera">00</p>
+              <p className="deta-detail-pera">days</p>
             </div>
             <div className="design-days-men">
-              <h3>00</h3>
-              <p>days</p>
+              <p className="deta-detail-pera">00</p>
+              <p className="deta-detail-pera">days</p>
             </div>
             <div className="design-days-men">
-              <h3>00</h3>
-              <p>days</p>
+              <p className="deta-detail-pera">00</p>
+              <p className="deta-detail-pera">days</p>
             </div>
             <div className="design-days-men">
-              <h3>00</h3>
-              <p>days</p>
+              <p className="deta-detail-pera">00</p>
+              <p className="deta-detail-pera">days</p>
             </div>
           </div>
-          <div>
-            <h1 className="justify-content-center align-items-center text-align-center">20% OFF FOR ALL T- SHIRT COLLECTION</h1>
-            <p>
-              leorm ipsum dolor sit amat concepts adipisicing elit poro quision
-              leorm ipsum dolor sit amat concepts adipisicing elit poro quision
-              elit poro quision
-            </p>
-            <button>View More</button>
-          </div>
-        </div>
+        </section>
       </div>
     </>
   );
