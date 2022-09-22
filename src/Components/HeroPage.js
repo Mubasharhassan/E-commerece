a9e529bd345d11906e681dc95b983fc4c0c569c2
Mubasharhassan <mubisparkai0311@gmail.com>
2022-09-22 17:47:14 +0500

    code change today

--- src/Components/HeroPage.js
+++ src/Components/HeroPage.js
@@ -1,19 +1,18 @@
 import React from "react";
+import { CardText, Card, CardTitle, Button } from "reactstrap";
 
 const HeroPage = () => {
   return (
     <>
       <div className="container-fluid main-cards-detail justify-content-center align-items-center all-div-margn">
         <div className="back-img ">
-          <div className="detail ">
-            <h2 className="livefor-text">Live For</h2>
-            <h1 className="fashion-text">Fashion</h1>
-            <p className="saveupto-text">Save Up To 50%</p>
-            <button
-            className="shopbtn"
-            >
-              Shop Now
-            </button>
+        <div className="hero-page-detail">
+          <h6 className="livefor-text">Live for</h6>
+          <h2 className="fashion-text">Fashion</h2>
+          <p className="saveupto-text">
+            Save up to 50%
+          </p>
+          <Button className="shopbtn">SHOP NOW</Button>
           </div>
         </div>
       </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,35 +8,38 @@ const Navbar = () => {
   return (
     <>
       <div className="container-fluid main-cards-detail justify-content-center align-items-center all-div-margn">
-        <header class="header">
-          <img src="/logo.jpeg" alt="trilo logo" class="logo" />
-          <form action="#" class="search">
-            <ul className="d-flex listitem">
-              <li className="m-5">Home</li>
-              <li className="m-5">Features</li>
-              <li className="m-5">Blog</li>
-              <li className="m-5">Shop</li>
-              <li className="m-5">Pages</li>
-            </ul>
-          </form>
+        <header class="header container">
+          <img src="/carryshop.png" alt="trilo logo" class="logo" />
+          <div className="navbar-detail-div">
+            <form action="#" class="search">
+              <ul className="d-flex listitem">
+                <li className="m-5">Home</li>
+                <li className="m-5">Features</li>
+                <li className="m-5">Blog</li>
+                <li className="m-5">Shop</li>
+               
+              </ul>
+            </form>
+          </div>
+
           <nav className="user-nav">
             <div className="user-nav__icon-box">
               <svg className="user-nav__icon">
-                <AiTwotoneHeart />
+                <AiTwotoneHeart className="icon-navbar" />
               </svg>
               <span className="user-nav__notification">7</span>
             </div>
             <div className="user-nav__icon-box">
               <svg className="user-nav__icon">
-                <FaShopify />
+                <FaShopify className="icon-navbar" />
               </svg>
               <span className="user-nav__notification">13</span>
             </div>
             <div className="user-nav__icon-box">
-              <BiSearchAlt2 />
+              <BiSearchAlt2 className="icon-navbar" />
             </div>
             <div className="user-nav__icon-box">
-              <GiHamburgerMenu />
+              <GiHamburgerMenu className="icon-navbar" />
             </div>
           </nav>
         </header>
